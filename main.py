d05c0f112df807211b934b229a9b8302068c5b67
shestakovitch <shestakovitch@gmail.com>
2025-04-03 16:17:28 +0200

    Added acive/inactive filter

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from config import url
 from scraper import fetch_page
 from data_collector import get_numbers
-# from time_parsing import parse_time
-
+from time_parsing import filter_numbers
+import json
 
 def main():
     try:
         # Загрузка страницы
         soup = fetch_page(url)
-        print(get_numbers(soup))
+        data = get_numbers(soup)  # Получаем список номеров
 
+        # Фильтруем данные
+        filtered_data = filter_numbers(data)
+
+        # Выводим отфильтрованные данные
+        print(json.dumps(filtered_data, indent=4, ensure_ascii=False))
 
     except Exception as e:
         print(f"Ошибка: {e}")
-
 
 if __name__ == "__main__":
     main()
