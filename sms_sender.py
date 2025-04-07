66f3b87ea0d219da96580ac9cc90134b1f74a2cb
shestakovitch <shestakovitch@gmail.com>
2025-04-07 20:40:16 +0200

    Added full comment to text_to_seconds

--- sms_sender.py
+++ sms_sender.py
@@ -1,31 +1,31 @@
-import requests
-from redis_client import redis_client
-
-phone = "447464143355"
-
-url = "https://dev.7sim.cc/in"
-params_add_number = {
-    "token": "sd232hasasdS",
-    "action": "newnum",
-    "phone": phone,
-    "country_code": "US",
-    "site": f"https://anonymsms.com/number/{phone}/"
-}
-
-params_send_sms = {
-    "token": "sd232hasasdS",
-    "action": "newsms",
-    "phone": phone,
-    "from": "TEST",
-    "message": "Follow the white rabbit",
-    "site": f"https://anonymsms.com/number/{phone}/"
-}
-
-
-response_add_number = requests.get(url=url, params=params_add_number)
-# print(response_add_number.status_code)
-# print(response_add_number.text)
-
-response_send_sms = requests.get(url=url, params=params_send_sms)
-print(response_send_sms.status_code)
-print(response_send_sms.text)
+# import requests
+# from redis_client import redis_client
+#
+# phone = "447464143355"
+#
+# url = "https://dev.7sim.cc/in"
+# params_add_number = {
+#     "token": "sd232hasasdS",
+#     "action": "newnum",
+#     "phone": phone,
+#     "country_code": country,
+#     "site": f"https://anonymsms.com/number/{phone}/"
+# }
+#
+# params_send_sms = {
+#     "token": "sd232hasasdS",
+#     "action": "newsms",
+#     "phone": phone,
+#     "from": "TEST",
+#     "message": text,
+#     "site": f"https://anonymsms.com/number/{phone}/"
+# }
+#
+#
+# response_add_number = requests.get(url=url, params=params_add_number)
+# # print(response_add_number.status_code)
+# # print(response_add_number.text)
+#
+# response_send_sms = requests.get(url=url, params=params_send_sms)
+# print(response_send_sms.status_code)
+# print(response_send_sms.text)
